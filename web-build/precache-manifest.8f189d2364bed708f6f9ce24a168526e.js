b4965567e9d0c6f87ce2d57d5561b9c335daa34c
Daksh0211 <55838915+Daksh0211@users.noreply.github.com>
2020-05-17 20:03:54 +0530

    Add files via upload

---
+++ web-build/precache-manifest.8f189d2364bed708f6f9ce24a168526e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "610c8cfd195d03a16bcb",
+    "url": "/static/js/app.5e94111d.chunk.js"
+  },
+  {
+    "revision": "12285de5044dece6d261",
+    "url": "/static/js/runtime~app.a6639e49.js"
+  },
+  {
+    "revision": "80b78de11984ef48209d",
+    "url": "/static/js/2.3d05eb30.chunk.js"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "afd5733ac789ab1795f1ed7a26e1ecd3",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "3d85359b37ca7590f8e91d593d02ddaf",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "5e52768b0f7cbb62cbe07751add33101",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "478f464c5f4270ccf3d48d3509f1a548",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5db41c0a82799281381afd76698a75ae",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "04cf1ae10e30155ca207400239ec1cbd",
+    "url": "/register-service-worker.js"
+  }
+];
